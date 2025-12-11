f18fe8f8bf7ee31ea8d27db6ba6c16959221e94a
nabeel <nabeel@192.168.1.28>
2025-12-12 01:13:32 +0530

    Record

--- Test Suites/New Test Suite Collection.ts
+++ Test Suites/New Test Suite Collection.ts
@@ -3,7 +3,7 @@
    <description></description>
    <name>New Test Suite Collection</name>
    <tag></tag>
-   <delayBetweenInstances>0</delayBetweenInstances>
+   <delayBetweenInstances>15</delayBetweenInstances>
    <executionMode>PARALLEL</executionMode>
    <maxConcurrentInstances>3</maxConcurrentInstances>
    <testSuiteRunConfigurations>
